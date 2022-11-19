2ecff0777efb0d3daa6e34459faa67c968c688f6
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2022-11-19 11:47:56 +0300

    'init;

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {TailwindProvider } from 'tailwind-rn';
+import Auth from './app/components/screens/Auth/Auth';
+import utilities from './tailwind.json';
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'PTSans': require('./assets/fonts/PTSans-Regular.ttf'),
+  });
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <TailwindProvider utilities={utilities}>
+        <Auth/>
+      </TailwindProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/components/screens/Auth/Auth.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native'
+import { useTailwind } from 'tailwind-rn'
+
+
+const Auth = () => {
+
+  const tailwind = useTailwind()
+
+  return (
+    <View style={styles.container}>
+        {/* <Image source={require("../../../assets/images/LOGO.png")} style={styles.ImageBackground}/> */}
+        <View>
+          <Text style={tailwind('text-center text-gray-800 text-2xl font-bold mb-2')}>Вход</Text>
+        </View>
+        <TextInput style={styles.input} placeholder='ИНН'/>
+        <TextInput style={styles.input} placeholder='Пароль'/>
+        <Button title="Войти" style={styles.button}></Button>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 40
+    },
+    ImageBackground: {
+        resizeMode: "contain",
+        width: 200,
+        height: 200,
+    },
+});
+
+
+export default Auth
